16843dd314a3118b9d1f68082f0833fb9351a9d6
sholabioku <bilushonline@gmail.com>
2023-01-06 17:47:37 +0100

    Add functionality to register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,35 @@
-const registerUser = (req, res) => {
-  if (!req.body.email) {
+const asyncHandler = require('express-async-handler');
+
+const User = require('../models/userModel');
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
     res.status(400);
-    throw new Error('Please add an email');
+    throw new Error('Please fill in all required fields');
   }
 
-  res.send('Register user');
-};
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error('Password must be at least 6 characters');
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('Email has already been registered');
+  }
+
+  const user = await User.create({ name, email, password });
+  if (user) {
+    const { _id, name, email, phone, photo, bio } = user;
+    res.status(201).json({ _id, name, photo, email, phone, bio });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 module.exports = {
   registerUser,
