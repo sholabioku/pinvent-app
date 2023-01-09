5ee9620b4948562fc6115f7457cf987d4c82c3fb
sholabioku <bilushonline@gmail.com>
2023-01-09 09:02:01 +0100

    Add functionality to logout user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,7 +85,19 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+const logout = asyncHandler(async (req, res) => {
+  res.cookie('token', {
+    path: '/',
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: 'none',
+    secure: true,
+  });
+  return res.status(200).json({ message: 'Successfully logout' });
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  logout,
 };
