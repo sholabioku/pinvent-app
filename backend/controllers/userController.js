7bc58d34478615be463c54e80350e53635b8ec5a
sholabioku <bilushonline@gmail.com>
2023-02-11 08:11:11 +0100

    Add functionality to change password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -148,7 +148,28 @@ const updateUser = asyncHandler(async (req, res) => {
 });
 
 const changePassword = asyncHandler(async (req, res) => {
-  res.send('Password changed');
+  const user = await User.findById(req.user._id);
+  const { oldPassword, password } = req.body;
+
+  if (!user) {
+    res.status(400);
+    throw new Error('User not found, please signup');
+  }
+
+  if (!oldPassword || !password) {
+    res.status(400);
+    throw new Error('Please add old and new password');
+  }
+
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+  if (user && passwordIsCorrect) {
+    user.password = password;
+    await user.save();
+    res.status(200).send('Password changed successful');
+  } else {
+    res.status(400);
+    throw new Error('Old Password is incorrect');
+  }
 });
 
 module.exports = {
