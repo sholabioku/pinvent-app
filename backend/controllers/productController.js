7643ad7f8f3eb1b74140d3adfeecf0c6ca53db43
sholabioku <bilushonline@gmail.com>
2023-02-13 15:17:13 +0100

    Add functionality create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,24 @@
 const asyncHandler = require('express-async-handler');
+const Product = require('../models/productModel');
 
 const createProduct = asyncHandler(async (req, res) => {
-  res.send('Product created');
+  const { name, sku, category, quantity, price, description } = req.body;
+  if (!name || !category || !quantity || !price || !description) {
+    res.status(400);
+    throw new Error('Please fill in all fileds');
+  }
+
+  const product = await Product.create({
+    user: req.user.id,
+    name,
+    category,
+    sku,
+    quantity,
+    price,
+    description,
+  });
+
+  res.status(201).json(product);
 });
 
 module.exports = { createProduct };
