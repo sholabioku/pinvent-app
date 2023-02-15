dc51e33f06d509650deb2d2809305ce8ae4950f3
sholabioku <bilushonline@gmail.com>
2023-02-15 09:39:51 +0100

    Add functionality to save file to cloudinary

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const Product = require('../models/productModel');
 const { fileSizeFormatter } = require('../utils/fileUpload');
+const cloudinary = require('cloudinary').v2;
 
 const createProduct = asyncHandler(async (req, res) => {
   const { name, sku, category, quantity, price, description } = req.body;
@@ -11,9 +12,20 @@ const createProduct = asyncHandler(async (req, res) => {
 
   let fileData = {};
   if (req.file) {
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'Pinvent App',
+        resource_type: 'image',
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error('Image could not be uploaded');
+    }
+
     fileData = {
       fileName: req.file.originalname,
-      filePath: req.file.path,
+      filePath: uploadedFile.secure_url,
       fileType: req.file.mimetype,
       filename: fileSizeFormatter(req.file.size, 2),
     };
