326a0ebb570ab784fca7fbde05b0d66e4abdc09c
sholabioku <bilushonline@gmail.com>
2023-02-16 07:42:57 +0100

    Add functionality to delete product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -59,10 +59,27 @@ const getProduct = asyncHandler(async (req, res) => {
 
   if (product.user.toString() !== req.user.id) {
     res.status(401);
-    throw new Error('User not authorized');
+    throw new Error('User not authorized to view product');
   }
 
   res.status(200).json(product);
 });
 
-module.exports = { createProduct, getProducts, getProduct };
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+
+  if (product.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('User not authorized to view product');
+  }
+
+  await product.remove();
+
+  res.status(200).json({ message: 'Product deleted successfully' });
+});
+
+module.exports = { createProduct, getProducts, getProduct, deleteProduct };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,6 +5,7 @@ const {
   createProduct,
   getProducts,
   getProduct,
+  deleteProduct,
 } = require('../controllers/productController');
 const protect = require('../middleware/authMiddleware');
 const { upload } = require('../utils/fileUpload');
@@ -12,5 +13,6 @@ const { upload } = require('../utils/fileUpload');
 router.post('/', protect, upload.single('image'), createProduct);
 router.get('/', protect, getProducts);
 router.get('/:id', protect, getProduct);
+router.delete('/:id', protect, deleteProduct);
 
 module.exports = router;
