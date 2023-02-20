66baa9a62d23bff210e12a6bed21576563519713
sholabioku <bilushonline@gmail.com>
2023-02-20 10:03:12 +0100

    Add jsx for navbar

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { RiProductHuntLine } from 'react-icons/ri';
+
+import './Home.scss';
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className='home'>
+      <nav className='container --flex-between'>
+        <div className='logo'>
+          <RiProductHuntLine size={35} />
+        </div>
+        <ul className='home-links'>
+          <li>
+            <Link to='/register'>Register</Link>
+          </li>
+          <li>
+            <button className='--btn --btn-primary'>
+              <Link to='/login'>Login</Link>
+            </button>
+          </li>
+          <li>
+            <button className='--btn --btn-primary'>
+              <Link to='/dashboard'>Dashboard</Link>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Home;
