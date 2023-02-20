0b437f638d3da02337ae1649c3a696933930cf4b
sholabioku <bilushonline@gmail.com>
2023-02-20 15:54:43 +0100

    Add jsx to create auth pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Forgot from './pages/auth/Forgot';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Reset from './pages/auth/Reset';
 import Home from './pages/Home/Home';
 
 function App() {
@@ -6,6 +10,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot' element={<Forgot />} />
+        <Route path='/resetpassword/:resetToken' element={<Reset />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/auth/Forgot.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Forgot = () => {
+  return <div>Forgot password</div>;
+};
+
+export default Forgot;
