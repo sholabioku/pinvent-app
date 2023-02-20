74daf8c8ec70c650b27ee4cbc39dd09aa1ee3e41
sholabioku <bilushonline@gmail.com>
2023-02-20 10:31:44 +0100

    Complete jsx for home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { RiProductHuntLine } from 'react-icons/ri';
 
+import heroImg from '../../assets/inv-img.png';
 import './Home.scss';
 
 const Home = () => {
@@ -27,6 +28,37 @@ const Home = () => {
           </li>
         </ul>
       </nav>
+      <section className='container hero'>
+        <div className='hero-text'>
+          <h2>Inventory & Stock Management Solution</h2>
+          <p>
+            Inventory system to control and manage products in the warehouse in
+            realtime and integrated to make it easier to develop your business.
+          </p>
+          <div className='hero-buttons'>
+            <button className='--btn --btn-secondary'>
+              <Link to='/dashboard'>Free Trial 1 Month</Link>
+            </button>
+          </div>
+          <div className='--flex-start'>
+            <NumberText num='14K' text='Brand Owner' />
+            <NumberText num='23K' text='Active Users' />
+            <NumberText num='500+' text='Partners' />
+          </div>
+        </div>
+        <div className='hero-image'>
+          <img src={heroImg} alt='Inventory' />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className='--mr'>
+      <h3 className='--color-white'>{num}</h3>
+      <p className='--color-white'>{text}</p>
     </div>
   );
 };
