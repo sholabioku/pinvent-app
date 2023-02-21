125e79b6cdafbc3ae8fe23395332d80196f01539
sholabioku <bilushonline@gmail.com>
2023-02-21 16:18:56 +0100

    Add markup for forgot password page

--- frontend/src/pages/auth/Forgot.jsx
+++ frontend/src/pages/auth/Forgot.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import Card from '../../components/card/Card';
+
+import styles from './auth.module.scss';
 
 const Forgot = () => {
-  return <div>Forgot password</div>;
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className='--flex-center'>
+            <AiOutlineMail size={35} color='#999' />
+          </div>
+          <h2>Forgot Password</h2>
+          <form>
+            <input type='email' placeholder='Email' name='email' required />
+            <button type='submit' className='--btn --btn-primary --btn-block'>
+              Get Reset Email
+            </button>
+            <div className={styles.links}>
+              <p>
+                <Link to='/'>- Home</Link>
+              </p>
+              <p>
+                <Link to='/login'>- Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Forgot;
