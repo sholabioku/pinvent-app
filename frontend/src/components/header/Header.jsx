0f0bcee911f7c567deea8151b2b680ae79c7642f
sholabioku <bilushonline@gmail.com>
2023-03-06 09:57:35 +0100

    Add functionality for logout user

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { SET_LOGIN } from '../../redux/features/auth/authSlice';
+import { logoutUser } from '../../services/authService';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logout = async () => {
+    await logoutUser();
+    await dispatch(SET_LOGIN(false));
+    navigate('/login');
+  };
+
   return (
     <div className='--pad header'>
       <div className='--flex-between'>
@@ -8,7 +21,9 @@ const Header = () => {
           <span className='--fw-thin'>Welcome, </span>
           <span className='--color-danger'>Bilush</span>
         </h3>
-        <button className='--btn --btn-danger'>Logout</button>
+        <button className='--btn --btn-danger' onClick={logout}>
+          Logout
+        </button>
       </div>
       <hr />
     </div>
