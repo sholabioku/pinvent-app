71e599d814cdab411c7868001a1bbae9b7ff5e8a
sholabioku <bilushonline@gmail.com>
2023-03-07 07:30:30 +0100

    Add functionality to display name

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-import { SET_LOGIN } from '../../redux/features/auth/authSlice';
+import { SET_LOGIN, selectName } from '../../redux/features/auth/authSlice';
 import { logoutUser } from '../../services/authService';
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const name = useSelector(selectName);
+
   const logout = async () => {
     await logoutUser();
     await dispatch(SET_LOGIN(false));
@@ -19,7 +21,7 @@ const Header = () => {
       <div className='--flex-between'>
         <h3>
           <span className='--fw-thin'>Welcome, </span>
-          <span className='--color-danger'>Bilush</span>
+          <span className='--color-danger'>{name}</span>
         </h3>
         <button className='--btn --btn-danger' onClick={logout}>
           Logout
