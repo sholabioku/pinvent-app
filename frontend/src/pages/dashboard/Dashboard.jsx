7b8e59b658ed0387a0a54c7037cf512627cdc7a4
sholabioku <bilushonline@gmail.com>
2023-03-08 07:53:18 +0100

    Add functionality for useRedirectLogOutUser

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
 
 const Dashboard = () => {
+  useRedirectLoggedOutUser('/login');
+
   return (
     <div>
       <h2>Dashboard</h2>
