0868fafcdee4498be454f620d312a57640d53413
sholabioku <bilushonline@gmail.com>
2023-03-24 10:56:43 +0100

    Add functionality for pagination

--- frontend/src/components/product/productList/ProductList.jsx
+++ frontend/src/components/product/productList/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 import { AiOutlineEye } from 'react-icons/ai';
+import ReactPaginate from 'react-paginate';
 
 import { SpinnerImg } from '../../loader/Loader';
 import './productList.scss';
@@ -24,6 +25,26 @@ const ProductList = ({ isLoading, products }) => {
     }
     return text;
   };
+
+  /* Begin Pagination */
+
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 2;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+
+    setCurrentItems(filteredProducts.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(filteredProducts.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, filteredProducts]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % filteredProducts.length;
+    setItemOffset(newOffset);
+  };
+  /* End Pagination */
 
   useEffect(() => {
     dispatch(FILTER_PRODUCTS({ products, search }));
@@ -64,7 +85,7 @@ const ProductList = ({ isLoading, products }) => {
                 </tr>
               </thead>
               <tbody>
-                {filteredProducts.map((product, index) => {
+                {currentItems.map((product, index) => {
                   const { _id, name, category, price, quantity } = product;
                   return (
                     <tr key={_id}>
@@ -98,6 +119,20 @@ const ProductList = ({ isLoading, products }) => {
             </table>
           )}
         </div>
+        <ReactPaginate
+          breakLabel='...'
+          nextLabel='Next'
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          pageCount={pageCount}
+          previousLabel='Prev'
+          renderOnZeroPageCount={null}
+          containerClassName='pagination'
+          pageLinkClassName='page-num'
+          previousLinkClassName='page-num'
+          nextLinkClassName='page-num'
+          activeLinkClassName='activePage'
+        />
       </div>
     </div>
   );
