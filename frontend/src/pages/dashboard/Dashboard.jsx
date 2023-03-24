f556c8d7060951d0a7eb1c6868e75798da5fce75
sholabioku <bilushonline@gmail.com>
2023-03-24 11:41:33 +0100

    Add jsx for ProductSummary

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ProductList from '../../components/product/productList/ProductList';
+import ProductSummary from '../../components/product/productSummary/ProductSummary';
 import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
 import { selectIsLoggedIn } from '../../redux/features/auth/authSlice';
 import { getProducts } from '../../redux/features/product/productSlice';
@@ -26,7 +27,7 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h2>Dashboard</h2>
+      <ProductSummary products={products} />
       <ProductList products={products} isLoading={isLoading} />
     </div>
   );
